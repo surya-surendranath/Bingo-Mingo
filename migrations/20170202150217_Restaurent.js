42566b2b06eaeaee4dcb4110649433e22cdc0078
surya-surendranath <surya.snath08@gmail.com>
2017-02-06 15:25:45 +1300

    Restaurent main page deal slides added for test

--- migrations/20170202150217_Restaurent.js
+++ migrations/20170202150217_Restaurent.js
@@ -1,8 +1,29 @@
-
 exports.up = function(knex, Promise) {
-  
+
+  console.log('create table')
+
+  return knex.schema.createTableIfNotExists('Restaurent', function(table) {
+      table.increments('id')
+      table.string('Vendor')
+      table.string('Title')
+      table.string('Deal_price')
+      table.string('Old_price')
+      table.string('Category')
+      table.string('Image1')
+      table.string('Image2')
+      table.string('Image3')
+      table.string('Image4')  
+      table.string('Rating')
+      table.string('people_bought')
+      table.string('People_Reviewed')
+      table.string('Valid')
+      table.timestamps()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('Restaurent').then(function () {
+    console.log('Restaurent table were dropped')
+  })
 };
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var Knex = require('knex')
+var config = require('../knexfile')
+var knex = Knex(config[process.env.NODE_ENV || 'development'])
+var db = require('../lib/db')(knex)
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bingo-Mingo' });
+	db.getAll('Restaurent',function(err,data) {
+		res.render('index', { Restaurent:data });
+		console.log(data)
+	})
+  
 });
 
 module.exports = router;
+
